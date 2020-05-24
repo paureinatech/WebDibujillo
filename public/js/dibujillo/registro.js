fdfbe4284cdc041d8dc46815d14a7cceba41e212
Turbica <723883@unizar.es>
2020-05-24 14:33:53 +0200

    Travis

--- public/js/dibujillo/registro.js
+++ public/js/dibujillo/registro.js
@@ -10,7 +10,7 @@ var firebaseConfig = {
     measurementId: "G-9700NMW0BJ"
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+var firebase = firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 var firestore = firebase.firestore();
 
