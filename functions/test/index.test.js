85b079f32c3b9f177634c78c2fe7ab878f913829
Turbica <723883@unizar.es>
2020-05-24 13:49:43 +0200

    Travis

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,9 +1,9 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+// At the top of test/index.test.js
+const test = require('firebase-functions-test')({
+    databaseURL: 'https://dibujillo.firebaseio.com',
+    storageBucket: 'dibujillo.appspot.com',
+    projectId: 'dibujillo'
+}, '../../dibujillo-a9ae5eb0eef8.json');
 
 function testFunctions() {
     console.log('Test pasado');
