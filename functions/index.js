28b8b3f72676d4c010601981889dda440ee6515d
Turbica <723883@unizar.es>
2020-05-26 01:10:36 +0200

    Travis

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,4 @@
 const functions = require('firebase-functions');
-const exec  = require('child_process').exec;
-
-exec('wget https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js', function(stdout) { console.log('Firebase cargado'); });
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDzJ50PyRubhuR2I3dSBUcS70rYpi5FV9M",
-    authDomain: "dibujillo.firebaseapp.com",
-    databaseURL: "https://dibujillo.firebaseio.com",
-    projectId: "dibujillo",
-    storageBucket: "dibujillo.appspot.com",
-    messagingSenderId: "695183399338",
-    appId: "1:695183399338:web:d43ad736ccdaf6c20833d3",
-    measurementId: "G-9700NMW0BJ"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -32,57 +13,3 @@ firebase.analytics();
       console.log('Usuario creado con ID: ', context.params.user, original.email);
       return snapshot.ref.parent.child('uppercase').set(uppercase);
     });*/
-
-exports.test = function () {
-    console.log('Test pasado con functions');
-}
-
-var email = 'prueba@gmail.com';
-var nickname = 'prueba';
-var password = '123456';
-
-exports.registerIn = async function signIn() {
-
-    if (!validateForm()) {
-        return;
-    }
-    else {
-      console.log('Registrando...');
-    }
-
-    var result = true;
-    await firebase.auth().createUserWithEmailAndPassword(email, password).catch( function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-
-      console.log('Error al registrar usuario');
-      console.log(errorMessage);
-      // ...
-      result = false;
-    });
-    if (result === true) {
-        console.log('Registro realizado con exito');
-        await firestore.collection("usuarios").doc(email).set({
-                        email: email,
-                        apodo: nickname,
-                        total_puntos: 0,
-                        photoUrl:
-                            'https://img.vixdata.io/pd/jpg-large/es/sites/default/files/btg/bodyart.batanga.com/files/7-simpaticos-tatuajes-de-llamas-y-alpacas.jpg',
-                        monedas: 0,
-                        colores: ["0xFF000000"],
-                        iconos: [],
-                        amigos: [],
-                        solicitudes: [],
-         });
-        window.location.replace("juegos.html");
-    }
-    else {
-        console.log('No se ha podido registrar correctamente');
-    }
-    return result;
-}
-
-function validateForm() {
-    return true;
-}
