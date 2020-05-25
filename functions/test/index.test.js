b1970d75ac22619ac7c56433ddd4af9f426ba0a5
jorgeili <jorgeiliarte@hotmail.es>
2020-05-25 19:24:16 +0200

    test travis 3

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -9,5 +9,20 @@ const myTest = require('../index.js');
 
 myTest.test();
 
-const myTest2 = require('../../public/js/dibujillo/registro.js');
-myTest2.test();
+var firebaseConfig = {
+    apiKey: "AIzaSyDzJ50PyRubhuR2I3dSBUcS70rYpi5FV9M",
+    authDomain: "dibujillo.firebaseapp.com",
+    databaseURL: "https://dibujillo.firebaseio.com",
+    projectId: "dibujillo",
+    storageBucket: "dibujillo.appspot.com",
+    messagingSenderId: "695183399338",
+    appId: "1:695183399338:web:d43ad736ccdaf6c20833d3",
+    measurementId: "G-9700NMW0BJ"
+};
+// Initialize Firebase
+var firebase = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var firestore = firebase.firestore();
+
+let myTest2 = require('../../public/js/dibujillo/registro.js');
+const wrapped = test.wrap(myTest2.test());
